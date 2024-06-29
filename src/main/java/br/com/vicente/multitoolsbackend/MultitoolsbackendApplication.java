acde38bd9fa540d2a50694bdbc2860b528201103
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2024-06-29 12:32:13 -0300

    first commit

---
+++ src/main/java/br/com/vicente/multitoolsbackend/MultitoolsbackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.multitoolsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitoolsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitoolsbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vicente/multitoolsbackend/MultitoolsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicente.multitoolsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitoolsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
